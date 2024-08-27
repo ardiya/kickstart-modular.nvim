4f4480a1893e0f7f8c909706485678f1b572df38
Ardiya, Aditya <aditya.ardiya@boeing.com>
2024-08-27 11:14:31 +0900

    nvim add noice

--- lua/custom/plugins/my_plugins.lua
+++ lua/custom/plugins/my_plugins.lua
@@ -47,6 +47,33 @@ return {
     version = '*',
     opts = {},
   },
+
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
+    },
+    config = function()
+      require('noice').setup {
+        lsp = {
+          override = {
+            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+            ['vim.lsp.util.stylize_markdown'] = true,
+            ['cmp.entry.get_documentation'] = true,
+          },
+        },
+        presets = {
+          bottom_search = true,
+          command_palette = true,
+          long_message_to_split = true,
+          inc_rename = false,
+          lsp_doc_border = false,
+        },
+      }
+    end,
+  },
 }
 
 -- vim: ts=2 sts=2 sw=2 et

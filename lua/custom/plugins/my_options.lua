44f9107dafa468658d534086c53f9276a595fa73
Aditya Ardiya <1798412+ardiya@users.noreply.github.com>
2024-10-26 01:35:23 +0900

    fix: font, been using old font for too long

--- lua/custom/plugins/my_options.lua
+++ lua/custom/plugins/my_options.lua
@@ -4,11 +4,7 @@ vim.opt.relativenumber = true
 -- neovide configs
 if vim.g.neovide then
   -- only set font in neovide, otherwise the terminal emulator would handle it
-  if vim.fn.has 'win32' == 1 then
-    vim.opt.guifont = 'FantasqueSansM Nerd Font Mono'
-  else
-    vim.opt.guifont = 'FantasqueSansMono Nerd Font Mono'
-  end
+  vim.opt.guifont = 'FantasqueSansM Nerd Font Mono'
 
   vim.g.neovide_cursor_vfx_mode = 'pixiedust'
   vim.g.neovide_cursor_antialiasing = true
